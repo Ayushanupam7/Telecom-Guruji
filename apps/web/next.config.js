c04a1937c74e0ac6958246b41d1fa0958fce0a29
Ayush Anupam <ayushanupamofficiL7@gmail.com>
2026-08-15 21:47:05 +0530

    modified:   apps/web/next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,22 @@
+const path = require('path');
+
 process.env.NEXT_DISABLE_SWC_LOCKFILE_PATCH = '1';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  transpilePackages: ['@signalhub/shared', '@signalhub/types', '@signalhub/validation'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
+
 
