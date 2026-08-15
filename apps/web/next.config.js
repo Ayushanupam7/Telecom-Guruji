f52307839d9064a98a2e3f52e565c82c695bb9d1
Ayush Anupam <ayushanupamofficiL7@gmail.com>
2026-08-15 21:44:39 +0530

    modified:   apps/web/next.config.js 	new file:   render.yaml

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,5 @@
+process.env.NEXT_DISABLE_SWC_LOCKFILE_PATCH = '1';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -5,3 +7,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
